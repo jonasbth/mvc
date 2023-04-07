ed7e199e66f285738615ba10e412625c0193796f
Jonas B <117373198+jonasbth@users.noreply.github.com>
2023-04-07 14:16:39 +0200

    Initial commit

---
+++ src/Controller/QuoteControllerJson.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteControllerJson
+{
+    static $quotes = [
+        "There is no place like 127.0.0.1",
+        "There are only 10 kinds of people in this world: those who know binary and those who don’t.",
+        "A programmers’s favourite hangout place: Foo Bar.",
+        "Why did the programmer quit his job? He didn’t get arrays.",
+        "0 is false and 1 is true, right? 1.",
+        "Why do Java programmers have to wear glasses? Because they don’t C#.",
+        "Real programmers count from 0.",
+        "['hip', 'hip']",
+        "#lego { display: block; }",
+        ".monarch { position: inherit; }",
+        ".hobbit { height: 50%;}",
+        "#titanic { float: none; bottom: 0; }",
+        "Why do programmers always mix up Halloween and Christmas? Because Oct 31 equals Dec 25."
+    ];
+
+    #[Route("/api/quote", name: "api_quote")]
+    public function jsonQuote(): JsonResponse
+    {
+        date_default_timezone_set('Europe/Stockholm');
+
+        $index = random_int(0, count(self::$quotes) - 1);
+
+        $data = [
+            'timestamp' => date("H:i:s"),
+            'date' => date("Y-m-d"),
+            'quote' => self::$quotes[$index]
+        ];
+
+        $response = new JsonResponse($data);
+
+        $encOptions = $response->getEncodingOptions() | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE;
+        $encOptions = $encOptions ^ JSON_HEX_APOS; // Don't escape "'"
+        
+        $response->setEncodingOptions($encOptions);
+        return $response;
+    }
+}
---
+++ src/Controller/ReportControllerTwig.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportControllerTwig extends AbstractController
+{
+    static $dieMap = [
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        5 => "five",
+        6 => "six"
+    ];
+
+    #[Route("/", name: "me")]
+    public function me(): Response
+    {
+        return $this->render('me.html.twig');
+    }
+
+    #[Route("/lucky", name: "lucky")]
+    public function lucky(): Response
+    {
+        $number = random_int(1, 6);
+
+        $data = [
+            'number' => $number,
+            'image'  => self::$dieMap[$number] . ".png"
+        ];
+
+        return $this->render('lucky.html.twig', $data);
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+}
